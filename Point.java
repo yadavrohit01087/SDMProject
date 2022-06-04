fc958ecd2a04b0a21d9e644ca81885a28a2da37e
vaibhavjadha <106860582+vaibhavjadha@users.noreply.github.com>
2022-06-04 17:10:03 +0530

    added attribute z;
    Added

--- Point.java
+++ Point.java
@@ -1,22 +1,23 @@
 import java.util.Scanner;
-
+//one attribute added;
 public class Point {
-	
+	private int z;
 	private int x;
 	private int y;
 	Scanner sc= new Scanner(System.in);
-	 
+	 //added z=0;
 	public Point()
 	{
 		x=0;
 		y=0;
+		z=0;
 	}
-  
-	public Point(int ax,int ay)
+         // added z=az;
+	public Point(int ax,int ay,int az)
 	{
 		x=ax;
 		y=ay;
-		
+		z=az;
 	}
 	
     public void setX(int ax)
